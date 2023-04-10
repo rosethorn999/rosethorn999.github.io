db3c62d35283cf82570c9e42f56b3c38d496f5b8
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2023-04-10 16:15:38 -0700

    fix: Update color for SEO

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'extend-red': '#F72A50',
+        'extend-red': '#FF1A42',
         'extend-teal': '#1EF7C0',
         'extend-yellow': '#F7E32A',
         'extend-blue': '#34C2FF',
