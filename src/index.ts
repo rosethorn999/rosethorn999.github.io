7fdc3de44c0ef057a04f44e36ddb300e560bd220
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2023-05-10 12:39:28 -0700

    feat: Update job period

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import './index.css';
 
-const calcCurrentJobPeriod = () => {
-  const current = new Date();
-  const trendmicroOnBoardingDate = new Date(2019, 10, 13);
-  const x = current.getFullYear() - trendmicroOnBoardingDate.getFullYear() - 1;
-  const y = Math.abs(
-    current.getMonth() + (12 - trendmicroOnBoardingDate.getMonth()) + 1
-  );
-  const elem =
-    window.document.querySelector<HTMLInputElement>('#currentJobPeriod');
-  if (elem) {
-    elem.innerText = `${x} yrs ${y} mos`;
-  }
-};
-calcCurrentJobPeriod();
+// const calcCurrentJobPeriod = () => {
+//   const current = new Date();
+//   const trendmicroOnBoardingDate = new Date(2019, 10, 13);
+//   const x = current.getFullYear() - trendmicroOnBoardingDate.getFullYear() - 1;
+//   const y = Math.abs(
+//     current.getMonth() + (12 - trendmicroOnBoardingDate.getMonth()) + 1
+//   );
+//   const elem =
+//     window.document.querySelector<HTMLInputElement>('#currentJobPeriod');
+//   if (elem) {
+//     elem.innerText = `${x} yrs ${y} mos`;
+//   }
+// };
+// calcCurrentJobPeriod();
