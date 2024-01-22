d5da6a46680365418674f0a3d26d671895621acb
rosethorn999 <24700793+rosethorn999@users.noreply.github.com>
2024-01-22 13:12:22 -0800

    feat: Add dark mode

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,25 @@ import './index.css';
 //   }
 // };
 // calcCurrentJobPeriod();
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches)
+) {
+  document.documentElement.classList.add('dark');
+} else {
+  document.documentElement.classList.remove('dark');
+}
+
+function switchTheme() {
+  if (localStorage.theme === 'dark') {
+    localStorage.theme = 'light';
+    document.documentElement.classList.remove('dark');
+  } else {
+    localStorage.theme = 'dark';
+    document.documentElement.classList.add('dark');
+  }
+}
+document.querySelector('#themeSwitcher').addEventListener('click', switchTheme);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
+  darkMode: 'class',
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
         'extend-red': '#FF1A42',
         'extend-teal': '#1EF7C0',
-        'extend-yellow': '#F7E32A',
-        'extend-blue': '#34C2FF',
+        'extend-dark-shadow': '#f72a50',
       },
     },
   },
